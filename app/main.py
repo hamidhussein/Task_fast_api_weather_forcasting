03fa0a04668d9fa16f2349a94a0639e0e234368a
Hamid <hamid.pn@uswagb.edu.pk>
2025-08-14 00:52:44 +0500

    Resolved Error

--- app/main.py
+++ app/main.py
@@ -2,11 +2,14 @@ from fastapi import FastAPI
 from .routers import auth as auth_router
 from .routers import weather as weather_router
 
-app = FastAPI(title="Task_fast_api_weather", version="1.0.0")
+# Initialize the FastAPI app
+app = FastAPI(title="Task_fast_api_weather_forecasting", version="1.0.0")
 
+# Include the auth and weather routers (APIs)
 app.include_router(auth_router.router)
 app.include_router(weather_router.router)
 
+# Health check endpoint
 @app.get("/", tags=["meta"])
 def root():
-    return {"name": "Task_fast_api_weather", "status": "ok"}
+    return {"name": "Task_fast_api_weather_forecasting", "status": "ok"}
